36614b2eded3c1c910af2010c8a9f7d214250c7d
Aamna1449 <parveenaamna786@gmail.com>
2026-08-18 11:02:45 +0530

    Add files via upload

---
+++ FactorialLargeNumber.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+  public:
+    vector<int> factorial(int n) {
+        
+        vector<int>ans(1,1);
+        while(n>1){
+            int carry=0,res,size=ans.size();
+            for(int i=0;i<size;i++){
+                res=ans[i]*n+carry;
+                carry=res/10;
+                ans[i]=res%10;
+            }
+            while(carry){
+                ans.push_back(carry%10);
+                carry/=10;
+            }
+            n--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<endl;
+       Solution obj;
+
+    vector<int> result = obj.factorial(n);
+
+    cout << "Factorial of a large number: ";
+
+    for(int digit : result) {
+        cout << digit;
+    }
+
+    cout << endl;
+
+    return 0;
+
+}
