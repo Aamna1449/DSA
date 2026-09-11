e73ac6facf3233435521f0ba60725942e075ceac
Aamna1449 <parveenaamna786@gmail.com>
2026-09-11 12:01:58 +0530

    Add files via upload

---
+++ Day28/lowerToUpperCAse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void LowertoUpper(string &str,int index){
+    if(index==-1){
+        return;
+    }
+
+    str[index]='A'+str[index]-'a';
+    LowertoUpper(str,index-1);
+}
+int main(){
+    string str="aamna";
+    LowertoUpper(str,4);
+    cout<<str;
+}
